37a49fe5cb441e2a04f52e398810541d5fb70bed
IllusionMan1212 <hisham.abourgheba@gmail.com>
2021-07-29 14:22:02 +0300

    backend: stub some posts routes to appease frontend

--- server-go/routes/postsHandler.go
+++ server-go/routes/postsHandler.go
@@ -1,8 +1,11 @@
 package routes
 
 import (
+	"fmt"
 	"illusionman1212/twatter-go/utils"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func DeletePost(w http.ResponseWriter, req *http.Request) {
@@ -14,11 +17,23 @@ func LikePost(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetPostsCount(w http.ResponseWriter, req *http.Request) {
-
+	utils.OkWithJSON(w, `{
+		"message": "Successfully fetched posts count",
+		"status": "200",
+		"success": true,
+		"count": 0
+	}`)
 }
 
 func GetPosts(w http.ResponseWriter, req *http.Request) {
 	// TODO: fetch posts from the db
+	params := mux.Vars(req)
+
+	page := params["page"]
+	userId := params["userId"]
+
+	fmt.Printf("page %v\n", page)
+	fmt.Printf("userid %v\n", userId)
 
 	utils.OkWithJSON(w, `{
 		"message": "Successfully fetched posts",
